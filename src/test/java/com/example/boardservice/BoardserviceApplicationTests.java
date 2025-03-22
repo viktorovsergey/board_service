5f0e5e349a362c6d7478ab2b06a274bd88570efe
User <ya_zabitaka@bk.ru>
2025-03-22 13:51:04 +0300

    initial commit

---
+++ src/test/java/com/example/boardservice/BoardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
