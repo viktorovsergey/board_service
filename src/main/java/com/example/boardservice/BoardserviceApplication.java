a77016a4feb00898ee2a358ea207845c179630e0
User <ya_zabitaka@bk.ru>
2025-03-22 13:51:04 +0300

    initial commit

---
+++ src/main/java/com/example/boardservice/BoardserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardserviceApplication.class, args);
+    }
+
+}
